7a3ba9a8f1db982a4b7dd41f91ca5f3c87fdd312
terenceliew <liew@etud.insa-toulouse.fr>
2020-05-20 13:51:26 +0200

    normal

--- src/mydevices.cpp
+++ src/mydevices.cpp
@@ -9,81 +9,9 @@ int detectedfreqRFID = 0;
 int wrong_pwd = 0;
 int wrong_fp = 0;
 
+//test 2
 using namespace std;
-<<<<<<< HEAD
-//Actuator
-Actuator::Actuator(int d):Device(),temps(d){
-}
-void Actuator::setTemps(int d){
-  temps = d;
-}
-int Actuator::getTemps(){
-  return temps;
-}
-//DigitalActuator
-DigitalActuator::DigitalActuator(int d):Actuator(d),state(OFF){
-}
-void DigitalActuator::setState(int s){
-  state = s;
-}
-int DigitalActuator::getState(){
-  return state;
-}
-//AnalogActuator
-AnalogActuator::AnalogActuator(int d):Actuator(d),val(0){
-}
-void AnalogActuator::setVal(int v){
-  val = v;
-}
-int AnalogActuator::getVal(){
-  return val;
-}
-//class Camera
-Camera::Camera(int d):DigitalActuator(d),capture(0){
-  setState(LOW);
-}
-void Camera::run(){
-  while(1){
-    if(ptrmem!=NULL) setState(*ptrmem);
-    if (getState()==LOW){
-      //cout << "((((Camera OFF))))\n";
-    }
-    else{
-    	//cout << "((((Camera ON))))\n"; 
-    if(Force>100) capture=1;   
-    sleep(getTemps());
-    }
-  }
-}
-//class LED
-LED::LED(int d):DigitalActuator(d){
-  setState(LOW);
-}
-void LED::run(){
-  	int last_state = LOW;
-
-  while(1){
-    if(ptrmem!=NULL) setState(*ptrmem);
-    if (getState()==LOW){
-      //cout << "((((eteint))))\n";      
-      if (last_state!=getState()){
-      	luminosite_environnement -= 50;
-      }
-    }
-    else{
-    	//cout << "((((allume))))\n";
-    	if (last_state!=getState()){
-    		luminosite_environnement += 50;
-      }	
-    }    
-    last_state=getState();
-    sleep(getTemps());
-    }
-
-}
-||||||| 42db8d8
-=======
-//Actuator
+//Actuatorss
 Actuator::Actuator(int d):Device(),temps(d){
 }
 void Actuator::setTemps(int d){
@@ -171,7 +99,6 @@ void Servo::run(){
     sleep(getTemps());
   }
 }
->>>>>>> 233f0d6cafd29b73e85a363b91b150d6dd13f596
 
 //classe ExternalDigitalSensorButton
 ExternalDigitalSensorButton::ExternalDigitalSensorButton(int t):Device(),state(OFF),temps(t){
